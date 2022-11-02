3b0af3c7a5d522ea093568ab9a01b828a88dc32e
dewaleolaoye <adewaleolaoye21@gmail.com>
2022-11-02 21:58:57 +0100

    feat: shared input component

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,12 @@
-import { Button } from "ui";
+import { Button, Input } from 'ui';
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
       <Button />
+
+      <Input />
     </div>
   );
 }
